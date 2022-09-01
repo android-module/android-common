dc86b0a183fd0b502d8845df4a26ffe2d37e7f20
Caldremch <caldremch@163.com>
2022-09-01 11:08:51 +0800

    feat:
    update databinding, version

--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
 # maven central publish
 #./gradlew -PremoteDep=true android-common:publishAllPublicationsToMavenRepository
+#./gradlew -PremoteDep=true android-common-adapter:publishAllPublicationsToMavenRepository
 #./gradlew -PremoteDep=true android-viewmodel:publishAllPublicationsToMavenRepository
-./gradlew -PremoteDep=true android-common-adapter:publishAllPublicationsToMavenRepository
+./gradlew -PremoteDep=true android-common-databinding:publishAllPublicationsToMavenRepository
 
 
 # aliyun publish
